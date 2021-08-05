3c6a60de72d17f5a7a8270072e0d61906d54f344
Jieun Chon <g471000@gmail.com>
2021-08-05 21:15:13 +0900

    Kotlin List exercise

---
+++ Kotlins/Lists/listLoop.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val guestsPerFamily = listOf(2, 4, 1, 3)
+	var totalGuests = 0
+	var index = 0
+	while (index < guestsPerFamily.size) {
+	    totalGuests += guestsPerFamily[index]
+	    index++
+	}
+	println("Total Guest Count: $totalGuests")
+    
+	val names = listOf("Jessica", "Henry", "Alicia", "Jose")
+    for (name in names) {
+    	println("$name - Number of characters: ${name.length}")
+	}
+}
